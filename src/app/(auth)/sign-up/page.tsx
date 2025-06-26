89360f6a2fe0a1749af9258837f66e7eb918a417
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-06-26 10:39:53 +0530

    first push

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/app/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <AuthForm type="sign-up"/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/components/AuthForm.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from "next/link"
+import { toast } from "sonner"
+
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import Image from "next/image"
+
+
+const authFormScheme = (type: FormType) => {
+    return z.object({
+        name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(8),
+    })
+}
+
+
+const AuthForm = ({type} : { type: FormType}) => {
+    const formSchema = authFormScheme(type)
+
+    // Define the form using default values and validation schema.
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { 
+      name: "",
+      email: "",
+      password: "",
+    },
+  })
+ 
+    // Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        try{
+            if(type === "sign-up") {    
+                console.log("SIGN UP", values);
+            } else if(type === "sign-in") {
+                console.log("SIGN IN", values);
+            }
+        } catch (error) {
+            console.error("Error submitting form:", error);
+            toast.error("Something went wrong, please try again later.");
+        }
+    }
+
+    const isSignIn = type === "sign-in"; 
+  return (
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+                <Image
+                    src="/logo.png"
+                    alt="Logo"
+                    width={38}
+                    height={32}
+                    className="rounded-full"
+                />
+                <h2 className="text-primary-100">HireLyze</h2>
+            </div> 
+            <h3>Practice. Perfect. Perform — with AI by Your Side.</h3>
+            
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                {!isSignIn && <p>Name</p>}
+                <p>Email</p>
+                <p>Password</p>
+
+                <Button className="btn" type="submit">{isSignIn? 'Sign In':'Create an Account'}</Button>
+            </form>
+            </Form>
+
+            <p className="text-center">
+                {isSignIn ? 'No Account yet?' : 'Have an account already?'}
+                <Link href={isSignIn ? '/sign-up' : '/sign-in'} className="text-user-primary font-bold ml-1">
+                {isSignIn ? 'Sign Up' : 'Sign In'}
+                </Link>  
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default AuthForm
