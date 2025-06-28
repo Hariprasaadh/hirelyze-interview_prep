aa88242c63152eb779d282b1e934d777159ddd7c
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-06-28 17:39:52 +0530

    interview feature

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from "@/app/components/Agent"
+
+const page = () => {
+  return (
+    <div>
+      <h3>Interview Generation</h3>
+      <Agent userName="You" userId="user1" type="generate"></Agent>
+    </div>
+  )
+}
+
+export default page
